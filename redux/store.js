d4e17c5fe97de26ab2a5783a2a86b343bf9365d9
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-04-29 22:40:01 +0200

    delete userslice

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './productsSlice';
 import reviewReducer from './reviewsSlice';
-import userReducer from './userSlice';
 
 export const store = configureStore({
 	reducer: {
 		products: productsReducer,
 		reviews: reviewReducer,
-		user: userReducer,
 	},
 });
